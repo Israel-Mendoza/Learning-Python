ce116dfd3380611ba619855db876be31b3ea827f
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-13 13:49:19 -0600

    Update Descriptors4 - Working with Non-Data Descriptors 2.py
    Added annotations and showed how the Descriptor instance is shared among the different instances of the Rocket class.

--- Descriptors/1 - Descriptors/Descriptors4 - Working with Non-Data Descriptors 2.py
+++ Descriptors/1 - Descriptors/Descriptors4 - Working with Non-Data Descriptors 2.py
@@ -1,20 +1,28 @@
+from __future__ import annotations
+
+
 class Countdown:
-    """Non-data descriptor."""
+    """Non-data descriptor"""
 
-    def __init__(self, start):
-        self.start = start + 1
+    def __init__(self, start: int) -> None:
+        self.start: int = start + 1
+        self.current_value: int = self.start
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: any, owner: type):
         """
-        Returns the descriptor instance if called
-        from a class. 
+        Returns the descriptor instance if called from a class. 
         Otherwise, will decrease the self.start value 
         by one and return the value.
         """
         if instance is None:
             return self
-        self.start -= 1
-        return self.start
+        if self.current_value > 0:
+            # Current value may decrease.
+            self.current_value -= 1
+        else: 
+            # Current value has reached 0. Resetting it.
+            self.current_value = self.start - 1
+        return self.current_value
 
 
 class Rocket:
@@ -24,16 +32,32 @@ class Rocket:
     countdown = Countdown(10)
 
 
-r1 = Rocket()
-r2 = Rocket()
+# Creating two instances of Rocket.
+r1: Rocket = Rocket()
+r2: Rocket = Rocket()
+r3: Rocket = Rocket()
 
+
+# Notice how the Countdown instance is shared among all Rocket instances:
 print(f"{r1.countdown = }") # r1.countdown = 10
-print(f"{r1.countdown = }") # r1.countdown = 9
-print(f"{r1.countdown = }") # r1.countdown = 8
+print(f"{r2.countdown = }") # r2.countdown = 9
+print(f"{r3.countdown = }") # r3.countdown = 8
 print(f"{r1.countdown = }") # r1.countdown = 7
-print(f"{r1.countdown = }") # r1.countdown = 6
-print(f"{r1.countdown = }") # r1.countdown = 5
+print(f"{r2.countdown = }") # r2.countdown = 6
+print(f"{r3.countdown = }") # r3.countdown = 5
 print(f"{r1.countdown = }") # r1.countdown = 4
-print(f"{r1.countdown = }") # r1.countdown = 3
-print(f"{r1.countdown = }") # r1.countdown = 2
+print(f"{r2.countdown = }") # r2.countdown = 3
+print(f"{r3.countdown = }") # r3.countdown = 2
 print(f"{r1.countdown = }") # r1.countdown = 1
+print(f"{r2.countdown = }") # r2.countdown = 0
+print(f"{r3.countdown = }") # r3.countdown = 10
+print(f"{r1.countdown = }") # r1.countdown = 9
+print(f"{r2.countdown = }") # r2.countdown = 8
+print(f"{r3.countdown = }") # r3.countdown = 7
+print(f"{r1.countdown = }") # r1.countdown = 6
+print(f"{r2.countdown = }") # r2.countdown = 5
+print(f"{r3.countdown = }") # r3.countdown = 4
+print(f"{r1.countdown = }") # r1.countdown = 3
+print(f"{r2.countdown = }") # r2.countdown = 2
+print(f"{r3.countdown = }") # r3.countdown = 1
+print(f"{r1.countdown = }") # r1.countdown = 0
