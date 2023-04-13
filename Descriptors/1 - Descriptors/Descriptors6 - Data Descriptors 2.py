6e2081af15ff0e91b0cb7bdffc657ddc838ac22f
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-13 14:17:57 -0600

    Update Descriptors6 - Data Descriptors 2.py
    Added annotations.

--- Descriptors/1 - Descriptors/Descriptors6 - Data Descriptors 2.py
+++ Descriptors/1 - Descriptors/Descriptors6 - Data Descriptors 2.py
@@ -1,23 +1,23 @@
+from __future__ import annotations
+
+
 """Data Desriptors"""
-
-
-from typing import Any
 
 
 class Descriptor:
 
-    def __init__(self, descriptor_value: Any) -> None:
+    def __init__(self, descriptor_value: str) -> None:
         """
         Initializing the value the Descriptor's 
         instance will hold and return.
         """
-        self.value = descriptor_value
+        self.value: str = descriptor_value
 
-    def __get__(self, instance, owner) -> Any:
+    def __get__(self, instance: any, owner: type) -> str:
         """Returns self.value, no mather who calls the descriptor"""
         return self.value
 
-    def __set__(self, instance: Any, value: Any) -> None:
+    def __set__(self, instance: any, value: str) -> None:
         """
         Prints the instance, the passed value, and
         sets the "value" attribute with the passed value
@@ -26,30 +26,38 @@ class Descriptor:
         print(f"Value: {value}")
         instance.value = value
 
+    def __repr__(self) -> str:
+        return f"Descriptor object @ {hex(id(self)).upper()}"
+
 
 class Person:
 
     eye_color = Descriptor("Brown")
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
+
+    def __repr__(self) -> str:
+        return f"Person {self.name} ({self.age}) @ address {hex(id(self)).upper()}."
 
 
-p = Person("Israel Mendoza", 25)
+p = Person("Israel Mendoza", 31)
+
 
 # Initial Person class' __dict__
 for k, v in vars(Person).items():
     print(f"{k:12} - {v}")
 print()
 # __module__   - __main__
-# eye_color    - <__main__.Descriptor object at 0x0167F5C8>  <<<--- DESCRIPTOR!!!
-# __init__     - <function Person.__init__ at 0x034B0190>
+# eye_color    - Descriptor object @ 0X7FFB59E46DD0             <<<--- DESCRIPTOR!!!
+# __init__     - <function Person.__init__ at 0x7f4a462f1260>
+# __repr__     - <function Person.__repr__ at 0x7f4a462f1300>
 # __dict__     - <attribute '__dict__' of 'Person' objects>
 # __weakref__  - <attribute '__weakref__' of 'Person' objects>
 # __doc__      - None
 
-# Overriding the "eye_color" class attribute with a string
+# Overriding the "eye_color" class attribute with a string.
 # Remember that the __set__ descriptor method will only be called
 # when accesed by a class instance, not a class.
 Person.eye_color = "Blue"
