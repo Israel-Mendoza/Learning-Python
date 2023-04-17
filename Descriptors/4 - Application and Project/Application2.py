fa5623bcd43d2cf1b380fd46ff247979d808dfd8
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-17 17:48:23 -0600

    Update Application2.py
    Improved code.

--- Descriptors/4 - Application and Project/Application2.py
+++ Descriptors/4 - Application and Project/Application2.py
@@ -1,45 +1,74 @@
+from __future__ import annotations
+
 """Applying descriptors to validate types"""
 
 from numbers import Real
 
 
 class ValidType:
-
-    def __init__(self, type_):
+    def __init__(self, type_1: type, type_2: type = None) -> None:
         """
-        Initialized the type that the descriptor
+        Initialized the type (or types) that the descriptor
         will validate.
+        A second type is optional.
         """
-        self._type = type_
+        self._type_1 = type_1
+        self._type_2 = type_2
 
-    def __set_name__(self, cls, name):
-        self.name = name
+    def __set_name__(self, cls: type, name: str) -> None:
+        self.name: str = name
 
-    def __set__(self, obj, value):
-        if isinstance(value, self._type):
+    def __set__(self, obj: object, value: object) -> None:
+        """
+        Checks whether the passed value if an instance of the valid type(s).
+        """
+        if self._type_2:
+            if isinstance(value, self._type_1) or isinstance(value, self._type_2):
+                obj.__dict__[self.name] = value
+                return
+            else:
+                types: str = f"'{self._type_1.__name__}' or '{self._type_2.__name__}'"
+                error: str = f"'{self.name}' must be of type {types}."
+                raise ValueError(error)
+        if isinstance(value, self._type_1):
             obj.__dict__[self.name] = value
+            return
         else:
-            raise ValueError(
-                f"{self.name} must be a valid {self._type.__name__}")
+            error: str = f"'{self.name}' must be of type '{self._type_1.__name__}'."
+            raise ValueError(error)
 
-    def __get__(self, obj, cls):
+    def __get__(self, obj: object, cls: type) -> any:
         if obj is None:
             return self
         return obj.__dict__.get(self.name, None)
 
 
 class Person:
+    name: ValidType = ValidType(str)
+    age: ValidType = ValidType(int)
+    height: ValidType = ValidType(int, float)
 
-    age = ValidType(int)
-    height = ValidType(Real)
-    tags = ValidType(list)
-    favorite_foods = ValidType(list)
+    def __init__(self, name: str, age: int, height: int | float):
+        self.name: str = name
+        self.age: int = age
+        self.height: int | float = height
+        print("Person instance was successfully created.")
 
-
-p = Person()
 
 try:
-    p.height = "hello"
+    p: Person = Person("Israel", 31, "1.76")
 except ValueError as error:
     print(error)
-# height must be a valid Real
+# 'height' must be of type 'int' or 'float'.
+
+try:
+    p: Person = Person("Israel", "31", 1.76)
+except ValueError as error:
+    print(error)
+# 'age' must be of type 'int'.
+
+try:
+    p: Person = Person("Israel", 31, 1.76)
+except ValueError as error:
+    print(error)
+# Person instance was successfully created.
