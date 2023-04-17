ac80ee44d5b8f9d33e2bfdb2f2038a2bf8f9f287
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-17 14:27:23 -0600

    Update Methods are Non-Data Descriptors 3 - Creating Methods.py
    Updated documentation.

--- Descriptors/3 - Methods/Methods are Non-Data Descriptors 3 - Creating Methods.py
+++ Descriptors/3 - Methods/Methods are Non-Data Descriptors 3 - Creating Methods.py
@@ -1,24 +1,31 @@
+from __future__ import annotations
+
 """Creating a method the descriptor way"""
 
+# In this example, we'll create a function that accepts two parameters:
+# 1. A class instance.
+# 2. A random argument (in this case, a string).
+# Then, we will create a NON-DATA Descriptor that takes a function in its __init__ method.
+# The __get__ method will return
+# 1. The function when called from the class
+# 2. The function converted into a Method bound to the calling instance.
 
-from typing import Union
 from types import FunctionType, MethodType
 
 
-def say_hello(this, message: str) -> None:
+# Creating a simple function
+def say_hello(this: object, message: str) -> None:
     print(f"{this.name} says '{message}'")
 
 
 class CallableDescriptor:
-
     def __init__(self, a_func: FunctionType) -> None:
         self.a_func: FunctionType = a_func
 
-    def __get__(self, obj, cls) -> Union[FunctionType, MethodType]:
+    def __get__(self, obj: object, cls: type) -> FunctionType | MethodType:
         """
-        Returns the self.a_func funtion when 
-        called from the class, and the MethodType 
-        version of self.a_func if called from the instance.
+        Returns the self.a_func function when called from the class, and the
+        Returns the MethodType version of self.a_func if called from the instance.
         """
         if obj is None:
             return self.a_func
@@ -26,29 +33,30 @@ class CallableDescriptor:
 
 
 class Person:
+    # A function if called from Person.
+    # A method if called from a Person instance.
+    say_somethig: CallableDescriptor = CallableDescriptor(say_hello)
 
-    # A function if called from Person, and
-    # a method if called from a Person instance.
-    say_somethig = CallableDescriptor(say_hello)
-
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name: str) -> None:
+        self.name: str = name
 
 
 p = Person("Israel")
 
-# Analyzing the '__init__' attribute:
+# Analyzing the '__init__' attribute from the class and from the instance.
+# This is just to see how a regular method looks like when created "normally".
 print(f"{type(Person.__init__) = }")
 # type(Person.__init__) = <class 'function'>
 print(f"{type(p.__init__) = }")
 # type(p.__init__) = <class 'method'>
 
-# Analyzing the 'say_something' attribute:
+# Analyzing the 'say_something' attribute, which is our CallableDescriptor descriptor:
 print(f"{type(Person.say_somethig) = }")
 # type(Person.say_somethig) = <class 'function'>
 print(f"{type(p.say_somethig) = }")
 # type(p.say_somethig) = <class 'method'>
 
+# Calling the function, passing the instance as the first argument:
 Person.say_somethig(p, "¡Buenos días!")
 # Israel says '¡Buenos días!'
 p.say_somethig("Bonjour!")
