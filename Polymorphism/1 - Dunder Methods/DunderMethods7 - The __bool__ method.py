391bad280d5757b672e2c9371633a1f0e8344c56
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-19 15:51:09 -0600

    Update DunderMethods7 - The __bool__ method.py
    Review.

--- Polymorphism/1 - Dunder Methods/DunderMethods7 - The __bool__ method.py
+++ Polymorphism/1 - Dunder Methods/DunderMethods7 - The __bool__ method.py
@@ -1,11 +1,11 @@
+from __future__ import annotations
+
 """Working with the __bool__ method"""
 
 
-# If the __bool__ method is not implemented,
-# it will fall back to __len__.
+# If the __bool__ method is not implemented, it will fall back to __len__.
 #
-# If none of them are implemented,
-# the object will always be True.
+# If none of them are implemented, the object will always be True.
 
 
 class MyClass:
@@ -19,27 +19,29 @@ print(f"Is 'obj' True or False: {bool(obj)}")  # True
 
 
 class MyList1:
-    def __init__(self, length: int):
-        self._length = length
+    def __init__(self: MyList1, length: int) -> None:
+        self._length: int = length
 
-    def __len__(self):
+    def __len__(self: MyList1) -> int:
         return self._length
 
 
-obj_1 = MyList1(0)
-obj_2 = MyList1(10)
+obj_1: MyList1 = MyList1(0)
+obj_2: MyList1 = MyList1(10)
 
 # __len__ has been implemented
 print(f"{bool(obj_1) = }")
 print(f"{bool(obj_2) = }")
+# bool(obj_1) = False
+# bool(obj_2) = True
 print()
 
 
 class MyList2:
-    def __init__(self, length: int) -> None:
-        self._length = length
+    def __init__(self: MyList2, length: int) -> None:
+        self._length: int = length
 
-    def __len__(self) -> int:
+    def __len__(self: MyList2) -> int:
         return self._length
 
     def __bool__(self) -> bool:
@@ -52,4 +54,8 @@ obj_2 = MyList2(10)
 
 # __len__ and __bool have been implemented
 print(f"{bool(obj_1) = }")
+# __bool__ called!
+# bool(obj_1) = False
 print(f"{bool(obj_2) = }")
+# __bool__ called!
+# bool(obj_2) = True
