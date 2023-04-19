4c5e9b0dc422754d1bb3b1550f19d5f74118863c
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-19 09:33:49 -0600

    Update WeakRef7 - Using __callback__ 1.py
    Review.

--- Descriptors/5 - Weak References/WeakRef7 - Using __callback__ 1.py
+++ Descriptors/5 - Weak References/WeakRef7 - Using __callback__ 1.py
@@ -1,4 +1,6 @@
-"""What can we do with the weakref.ref's __callback__ attribute?"""
+from __future__ import annotations
+
+"""Practical applications of the weakref's __callback__"""
 
 # We will be using function storedd in the weakref.ref's __callback__
 # class attribute to distroy the current weakreference instance when 
@@ -11,22 +13,21 @@ import weakref
 
 
 class Person:
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __init__(self: Person, name: str) -> None:
+        self.name: str = name
 
-    def __repr__(self) -> str:
-        return f"Person('{self.name}') at {hex(id(self)).upper()}"
+    def __repr__(self: Person) -> str:
+        return f"Person('{self.name}') @ {hex(id(self)).upper()}."
 
 
-def my_callback(a_weakref: weakref.ref) -> None:
-    # Iterating through the my_references list and
-    # deleting the passed weak references from the
-    # my_references list, thus deleting all remains to
-    # the object, which destruction will call this function.
-    for reference in my_references:
-        if reference is a_weakref:
-            print(f"Removing {a_weakref} from my_references!")
-            my_references.remove(reference)
+def my_callback(person_weakref: weakref.ref) -> None:
+    # Iterating through the references_list list (in the global scope)
+    # and deleting the passed weak references from it, thus deleting 
+    # all remains to the object, which destruction will call this function.
+    for reference in references_list:
+        if reference is person_weakref:
+            print(f"Removing {person_weakref} from my_references!")
+            references_list.remove(reference)
 
 
 p1 = Person("Israel")
@@ -34,25 +35,26 @@ p2 = Person("Arturo")
 # p3 points to the same object as p2:
 p3 = p2
 
-my_references = [weakref.ref(p1, my_callback), weakref.ref(p2, my_callback)]
+# Creating a list that will hold the created weakrefs:
+references_list: list[weakref.ref[Person]] = [weakref.ref(p1, my_callback), weakref.ref(p2, my_callback)]
 
-print(f"\nmy_references = \n{my_references}\n")
-# my_references = 
+print(f"\nreferences_list = \n{references_list}\n")
+# references_list = 
 # [<weakref at 0x7f82b8c699f0; to 'Person' at 0x7f82b8c6cfa0>, <weakref at 0x7f82b8c71db0; to 'Person' at 0x7f82b8c6cd60>]
 
 
 # The weakref's __callback__ is called because 
 # the only strong reference to the object is killed
 del p1
-# Removing <weakref at 0x7f82b8c699f0; dead> from my_references!
+# Removing <weakref at 0x7f82b8c699f0; dead> from references_list!
 
 # The weakref's __callback__ is not called 
 # because the reference is still alive with 'p3'
 del p2
 
 del p3
-# Removing <weakref at 0x7f82b8c71db0; dead> from my_references!
+# Removing <weakref at 0x7f82b8c71db0; dead> from references_list!
 
 # The weak references are now gone too!
-print(f"my_references = {my_references}")
-# my_references = []
+print(f"references_list = {references_list}")
+# references_list = []
