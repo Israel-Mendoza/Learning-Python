7b1e1d5d33bf22676bb4efabf4f16293e7153678
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-19 12:08:11 -0600

    Update Inheritance3 - Inheriting Class Attributes.py
    Review.

--- Inheritance/Inheritance3 - Inheriting Class Attributes.py
+++ Inheritance/Inheritance3 - Inheriting Class Attributes.py
@@ -1,27 +1,28 @@
-from typing import Union
+from __future__ import annotations
 
 
 class Account:
 
     apr = 3.0
 
-    def __init__(self, account_number: int, balance: Union[int, float]) -> None:
+    def __init__(self: Account, account_number: int, initial_balance: int | float) -> None:
         self.account_number: int = account_number
-        self.balance: Union[int, float] = balance
+        self.balance: int | float = initial_balance
         self.account_type: str = "Generic account"
 
-    def calculate_interest(self) -> str:
+    def calculate_interest(self: Account) -> str:
         # The apr shown will be the class attribute, regardless of the instance info
         return f"Calculating interest on account {self.account_type}: {type(self).apr}"
 
 
 class SavigsAccount(Account):
 
+    # Class attribute is overriden:
     apr = 5.0
 
-    def __init__(self, account_number: int, balance: Union[int, float]) -> None:
+    def __init__(self: SavigsAccount, account_number: int, initial_balance: int | float) -> None:
         self.account_number: int = account_number
-        self.balance: Union[int, float] = balance
+        self.balance: int | float = initial_balance
         self.account_type: str = "Savings account"
 
 
@@ -47,3 +48,7 @@ s2 = SavigsAccount(12334, 1000)
 
 print(s2.calculate_interest())
 # Calculating interest on account Savings account: 1000
+
+# But the base class' apr property remains as 3.0:
+print(a.calculate_interest())
+# Calculating interest on account Generic account: 3.0
