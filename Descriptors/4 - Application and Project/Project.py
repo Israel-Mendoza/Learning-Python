afec66dc7b651ad73b62cd3c155777deb18c154c
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-04-18 18:37:15 -0600

    Update Project.py
    Added examples, improved code, added annotations.

--- Descriptors/4 - Application and Project/Project.py
+++ Descriptors/4 - Application and Project/Project.py
@@ -1,96 +1,184 @@
-from types import FunctionType, MethodType
-from typing import Any, Type
+from __future__ import annotations
 
 
-class IntegerField:
+class Integer:
 
-    def __init__(self, min_value: int = None, max_value: int = None):
-        self._min = min_value
-        self._max = max_value
+    """Data-descriptor that will allow the instance attribute to store integers in a given range."""
 
-    def __set_name__(self, cls: Type, name: str) -> None:
+    def __init__(self, min_value: int, max_value: int) -> None:
+        if (
+            isinstance(min_value, int)
+            and isinstance(max_value, int)
+            and min_value < max_value
+        ):
+            self.min_value: int = min_value
+            self.max_value: int = max_value
+        else:
+            raise ValueError("Integer limits are two ints, one smaller than the other")
+        print("Integer descriptor instance successfully created.")
+
+    def __set_name__(self, _: type, attr_name: str) -> None:
+        # Storing the name of the attribute we'll use to
+        # write to and read from the instance's namespace.
+        self.attr_name: str = attr_name
+
+    def __set__(self, instance: object, new_value: int) -> None:
         """
-        __set_name__ method. Stores the descriptor's instance name.
+        Makes sure the value is an integer in between the previously set range.
         """
-        self.name = name
+        if not isinstance(new_value, int):
+            raise ValueError(f"'{self.attr_name}' must be if type 'int'.")
+        if self.min_value <= new_value <= self.max_value:
+            instance.__dict__[self.attr_name] = new_value
+        else:
+            raise ValueError(
+                f"'{self.attr_name}' must be >= {self.min_value} and <= {self.max_value}"
+            )
 
-    def __set__(self, obj: Any, value: int) -> None:
+    def __get__(self, instance: object, _: type) -> Integer | int:
         """
-        __set__ method. Stores the value in the passed object's __dict__
-        only if the value is within the valid ranges defined at the
-        descriptor's initialization time.
+        Returns the Integer instance if called from a class.
+        Returns a valid integer in between the ranges previously set,
+        provided the __set__ method has been used to set the attribute's value.
+        Otherwise, raises an "Attribute Error".
         """
-        if self._validator(value):
-            obj.__dict__[self.name] = value
-
-    def __get__(self, obj: Any, cls: Type) -> bool:
-        if obj is None:
+        if instance is None:
             return self
-        return obj.__dict__.get(self.name, None)
+        # value_to_return: int | None = getattr(instance, self.attr_name)
+        try:
+            value_to_return: int | None = instance.__dict__[self.attr_name]
+        except KeyError:
+            value_to_return = None
+        if value_to_return:
+            return value_to_return
+        else:
+            raise AttributeError(f"'{self.attr_name}' has not been set yet.")
 
-    def _validator(self, value):
-        if not isinstance(value, int):
-            raise ValueError(f"'{self.name}' must be of type int")
-        if self._min is not None and value < self._min:
-            raise ValueError(f"'{self.name}' can't be less than {self._min}")
-        if self._max is not None and value > self._max:
-            raise ValueError(f"'{self.name}' can't be more than {self._max}")
-        return True
 
+class String:
+    def __init__(self, min_length: int, max_length: int) -> None:
+        if String._min_and_max_validator(min_length, max_length):
+            self.min_length: int = min_length
+            self.max_length: int = max_length
+        else:
+            raise ValueError(
+                "String must be delimited by positive numbers, one smaller than the other."
+            )
+        print("String descriptor instance successfully created.")
 
-class CharField:
+    def __set_name__(self, owner: type, attr_name: str) -> None:
+        # Storing the name of the attribute we'll use to
+        # write to and read from the instance's namespace.
+        self.class_name: str = owner.__name__
+        self.attr_name: str = attr_name
 
-    def __init__(self, min_length: int = 0, max_length: int = None):
-        self._min = min_length
-        self._max = max_length
-
-    def __set_name__(self, cls: Type, name: str) -> None:
+    def __set__(self, instance: object, new_value: str) -> None:
         """
-        __set_name__ method. Stores the descriptor's instance name.
+        Makes sure the value is an integer in between the previously set range.
         """
-        self.name = name
+        if (
+            isinstance(new_value, str)
+            and self.min_length <= len(new_value.strip()) <= self.max_length
+        ):
+            instance.__dict__[self.attr_name] = new_value.strip()
+        else:
+            raise ValueError(
+                f"'{self.attr_name}' must be a string between {self.min_length} and {self.max_length} characters long."
+            )
 
-    def __set__(self, obj: Any, value: str) -> None:
+    def __get__(self, instance: object, _: type) -> str | String:
         """
-        __set__ method. Stores the value in the passed object's __dict__
-        only if the value's length is within the valid ranges defined 
-        at the descriptor's initialization time.
+        Returns the String instance if called from a class.
+        Returns a valid string, which length is in between the ranges previously set,
+        provided the __set__ method has been used to set the attribute's value.
+        Otherwise, raises an "Attribute Error".
         """
-        if self._validator(value):
-            obj.__dict__[self.name] = value
-
-    def __get__(self, obj: Any, cls: Type) -> Any:
-        if obj is None:
+        if instance is None:
             return self
-        return obj.__dict__.get(self.name, None)
+        try:
+            value_to_return: str | None = instance.__dict__[self.attr_name]
+        except KeyError:
+            value_to_return = None
+        if value_to_return is None:
+            raise AttributeError(f"'{self.attr_name}' has not been set yet.")
+        return value_to_return
 
-    def _validator(self, value: Any) -> bool:
-        if not isinstance(value, str):
-            raise ValueError(f"'{self.name}' must be of type str")
-        if self._min is not None and len(value) < self._min:
-            raise ValueError(f"Length of '{self.name}' can't be "
-                             f"less than {self._min}")
-        if self._max is not None and len(value) > self._max:
-            raise ValueError(f"Length of '{self.name}' can't be "
-                             f"more than {self._max}")
-        return True
-
-
-class Person:
-
-    name = CharField(3, 25)
-    age = IntegerField(3, 99)
-
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
-
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}('{self.name}', {self.age})"
+    @staticmethod
+    def _min_and_max_validator(num_1: int, num_2: int) -> bool:
+        """
+        Private static method, used by the __init__ method to initialize the String instance.
+        It makes sure the passed ranges are possitive ints, one langer than the other.
+        Returns True if the previous conditions apply, otherwise returns False.
+        """
+        if isinstance(num_1, int) and isinstance(num_2, int):
+            return num_1 < num_2 and num_1 >= 0
+        return False
 
 
 try:
-    p = Person("Isra", 28)
-    print(p)
-except BaseException as ex:
-    print(f"{type(ex).__name__}: {ex}")
+
+    class Person:
+        name: String = String(-2, 10)
+        age: Integer = Integer(18, 19)
+
+except ValueError as ex:
+    print(ex)
+# String must be delimited by positive numbers, one smaller than the other.
+
+try:
+
+    class Person:
+        name: String = String(0, "10")
+        age: Integer = Integer(18, 19)
+
+except ValueError as ex:
+    print(ex)
+# String must be delimited by positive numbers, one smaller than the other.
+
+try:
+
+    class Person:
+        name: String = String(0, 10)
+        age: Integer = Integer("18", 19)
+
+except ValueError as ex:
+    print(ex)
+# String descriptor instance successfully created.
+# Integer limits are two ints, one smaller than the other
+
+try:
+
+    class Person:
+        name: String = String(2, 10)
+        age: Integer = Integer(18, 19)
+
+except ValueError as ex:
+    print(ex)
+# String descriptor instance successfully created.
+# Integer descriptor instance successfully created.
+
+p: Person = Person()
+
+try:
+    print(p.name)
+except AttributeError as ex:
+    print(ex)
+# 'name' has not been set yet.
+
+try:
+    p.name = ""
+except ValueError as ex:
+    print(ex)
+# 'name' must be a string between 2 and 10 characters long.
+
+try:
+    p.age
+except AttributeError as ex:
+    print(ex)
+# 'age' has not been set yet.
+
+try:
+    p.age = 15
+except ValueError as ex:
+    print(ex)
+# 'age' must be >= 18 and <= 19
