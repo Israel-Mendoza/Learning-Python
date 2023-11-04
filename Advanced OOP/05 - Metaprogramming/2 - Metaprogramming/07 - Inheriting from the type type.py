1b5105613061bf1f3c8c3d75d82bd76293a37451
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-11-03 23:31:26 -0600

    First use of metaclasses

--- Advanced OOP/05 - Metaprogramming/2 - Metaprogramming/07 - Inheriting from the type type.py
+++ Advanced OOP/05 - Metaprogramming/2 - Metaprogramming/07 - Inheriting from the type type.py
@@ -1,39 +1,46 @@
+from __future__ import annotations
+from typing import Any
+
 """Creating a class creator by inheriting from type"""
 
-# Let's create a class than inherits directly from type.
-# This class will be in charge of creating classes using the
-# super().__new__() method.
-# By doing this, we can customize how a class is created.
-# This will be our first example of a metaclass.
+"""
+    Let's create a class than inherits directly from type.
+    
+    The result will be a new "type" type, and we can use it
+    to create new classes. 
+    
+    This class will be in charge of creating classes using the
+    super().__new__() method.
+    By doing this, we can customize how a class is created.
+    This will be our first example of a metaclass.
+"""
 
 
-from typing import Type
+class CustomType(type):  # Inherits from "type"
 
+    """Metaclass that allows customization of class creation"""
 
-class CustomType(type):
-    """Metaclass  which allows customization of class creation"""
-
-    def __new__(cls: Type, name: str, bases: tuple, class_dict: dict) -> "CustomType":
+    def __new__(cls: type, name: str, bases: tuple, class_dict: dict) -> CustomType:
         """
         Called when CustomType is called, as expected.
         It creates a CustomType instance (a class) and hardcodes an instance method
         to this class before returning it.
         """
-        print("Creating a CustomeType type!")
+        print("Creating a CustomType type!")
         print(f"Class name: {class_name}")
         print(f"Class namespace: {class_dict}")
         # Updating namespace dictionary before instance creation
         class_dict["say_hello"] = lambda self: f"{self} says 'Hello, World!'"
-        instance = super().__new__(cls, name, bases, class_dict)
+        instance: CustomType = super().__new__(cls, name, bases, class_dict)
         print(f"CustomType type created at address {hex(id(instance)).upper()}")
-        # Updating before instance returning it
+        # We can customize the class object before returning it, but we've done it already:
         # instance.say_hello = lambda self: f"{self} says 'Hello, World!'"
         return instance
 
 
 """Using the CustomType metaclass to create classes"""
 
-# Preparing the arguments
+# Preparing the class components:
 class_name = "Person"
 class_bases = tuple()
 class_body = """
@@ -44,13 +51,13 @@ def __init__(self, name):
 def __str__(self):
     return f"Person called {self.name}"
 """
-class_namespace = {}
+class_namespace: dict[str, Any] = {}
 exec(class_body, globals(), class_namespace)
 
 # Using our CustomType class to create another class
-Person = CustomType(class_name, class_bases, class_namespace)
+Person: CustomType = CustomType(class_name, class_bases, class_namespace)
 
-# Instrospecting our new type
+# Introspecting our new type
 print(type(Person))  # <class '__main__.CustomType'>
 print(isinstance(Person, CustomType))  # True
 print(isinstance(Person, type))  # True
@@ -67,5 +74,5 @@ print(Person.__dict__)
 
 p1 = Person("Israel")
 # __init__ called!
-print(p1.say_hello())
+print(p1.say_hello())  # Method assigned via the metaclass "CustomType"
 # Person called Israel says 'Hello, World!'
--- Advanced OOP/05 - Metaprogramming/2 - Metaprogramming/08 - Using a metaclass to create classes.py
+++ Advanced OOP/05 - Metaprogramming/2 - Metaprogramming/08 - Using a metaclass to create classes.py
@@ -1,43 +1,41 @@
+from __future__ import annotations
+from typing import Any
+
 """
 Metaclasses are classes that are use to create other classes.
 Remember that, in order to create a class using type(), we need:
     1. The name of the class
-    2. The class bases the new class will inherit from
-    3. The namespace of the class.
+    2. The class bases the new class will inherit from (squeezed into a tuple)
+    3. The namespace of the class (a dictionary)
 """
 
 
-from typing import Any
-
-
 class CustomType(type):
-    def __new__(
-        cls, name: str, bases: tuple, namespace: dict[str, Any]
-    ) -> "CustomType":
+    def __new__(cls, name: str, bases: tuple, namespace: dict[str, Any]) -> CustomType:
         print(f"Creating the {name} class.")
-        new_class = super().__new__(cls, name, bases, namespace)
+        new_class: CustomType = super().__new__(cls, name, bases, namespace)
         new_class.say_hello = lambda self: f"{self} says: Hello World!"
         return new_class
 
 
-class Person(metaclass=type):
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
+class Person(metaclass=type):  # metaclass=type is the default
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
 
-    def introduce(self):
+    def introduce(self) -> None:
         print(f"My name is {self.name} and I'm {self.age} years old.")
 
 
-class Student(Person, metaclass=CustomType):
-    def study(self):
+class Student(Person, metaclass=CustomType):  # We'll use the CustomType metaclass to create "Student"
+    def study(self) -> None:
         print(f"I'm {self.name} and I study hard!")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Student called {self.name}"
 
 
-# Creating the Student class.
+"""Creating the Student class."""
 
 print(f"Class name: {Person.__name__} - Type: {type(Person)}")
 # Class name: Person - Type: <class 'type'>
